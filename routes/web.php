f827f469ea191ba3a90940b820957783b4691584
KB885 <78407625+KB885@users.noreply.github.com>
2022-03-10 14:57:06 +0100

    🚀 Update the routes
    Added routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
-use App\Http\Controllers\ArticleController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Auth\AuthenticationController;
 
 Route::controller(ArticleController::class)->group(function () {
     Route::get('/', 'index')->name('article.index');
+});
+
+Route::controller(AuthenticationController::class)->group(function () {
+    Route::get('/login', 'index')->name('login.index');
 });
